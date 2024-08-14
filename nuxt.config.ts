8a728290bbdf33f1b252a100f193be876180ac5e
alexoesmith <asmith@oomphworks.com>
2024-08-14 10:59:02 +0100

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: {
-    name: "Oomph Theme Layer",
+    name: "Lyles Coaches",
     defaultLocale: "en",
   },
 
