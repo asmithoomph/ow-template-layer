f742a331d420cc2dd1533cf814a68ebcebcd2d33
alexoesmith <asmith@oomphworks.com>
2024-08-14 11:34:10 +0100

    Oomph Branding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: {
-    name: "Lyles Coaches",
+    name: "Oomph Template Layer",
     defaultLocale: "en",
   },
 
